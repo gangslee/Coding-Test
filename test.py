88c87542907adbecc6a2ad5ee865218d8093b695
gangslee <grandnet1225@gmail.com>
2020-08-29 13:59:39 +0900

    0829 python

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-a = input()
-if a == 'm' or 'f':
-    print('123')
-else:
-    print('asdas')
+p = ['119', '97674223', '1195524421']
+
+def solution(phone_book):
+    
+    for i in range(len(phone_book)):
+        for j in range(i+1, len(phone_book)):
+            
+            if (phone_book[i] in phone_book[j]):
+                return False
+                
+
+    
+    return True
+
+solution(p)
